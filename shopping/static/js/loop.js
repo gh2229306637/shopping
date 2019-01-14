5b9f61dbbe78970f5ace041b171befcb91aff643
yangyang-git <18501970795@163.com>
2019-01-14 10:59:22 +0800

    goodslist2

--- shopping/static/js/loop.js
+++ shopping/static/js/loop.js
@@ -1,11 +1,12 @@
 var swiper = new Swiper('.swiper-container', {
-            pagination: '.swiper-pagination',
-            nextButton: '.swiper-button-next',
-            prevButton: '.swiper-button-prev',
-            slidesPerView: 1,
-            paginationClickable: true,
-            spaceBetween: 30,
-            loop: true,
-            autoplay: 3000,
-            autoplayDisableOnInteraction: false
-        });
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    slidesPerView: 1,
+    paginationClickable: true,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: 3000,
+    autoplayDisableOnInteraction: false
+});
+
